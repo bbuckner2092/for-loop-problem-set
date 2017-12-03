b3f0c22d231dfde06a7c5a86028877d8787cd287
Brelon Buckner <brelonbuckner@Brelons-MacBook-Air.local>
2017-12-03 12:51:59 -0500

    second for loop with an if statement

--- forLoop.js
+++ forLoop.js
@@ -1,1 +1,10 @@
-forLoop.js
+console.log("Print all numbers between -10 and 19");
+for (var i = -10; i < 20; i++)
+console.log(i);
+
+console.log("Print all even numbers between 10 and 40");
+for (var i = 10; i < 41; i++) 
+	if (i % 2 === 0) {
+		console.log(i);
+	}
+
